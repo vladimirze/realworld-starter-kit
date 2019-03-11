84d3f5ccc7d09c17258f916010a898d2dd95f1fb
Vladimir Zeifman <edoomik@gmail.com>
2019-03-11 11:11:32 +0200

    Updates

---
+++ precache-manifest.39b9e432e7eceef37852bd21034248f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39ed36e11a499d12ca33",
+    "url": "/vladimirze/realworld-starter-kit/static/js/runtime~main.39ed36e1.js"
+  },
+  {
+    "revision": "140cfd9967f337a1e183",
+    "url": "/vladimirze/realworld-starter-kit/static/js/main.140cfd99.chunk.js"
+  },
+  {
+    "revision": "e97ff5cfbe52f8aa82bb",
+    "url": "/vladimirze/realworld-starter-kit/static/js/1.e97ff5cf.chunk.js"
+  },
+  {
+    "revision": "140cfd9967f337a1e183",
+    "url": "/vladimirze/realworld-starter-kit/static/css/main.c7d7d045.chunk.css"
+  },
+  {
+    "revision": "9129e8081f06d20da11ffd293f2f257d",
+    "url": "/vladimirze/realworld-starter-kit/index.html"
+  }
+];
